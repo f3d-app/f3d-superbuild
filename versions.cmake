ef90a8a6528912f168f852e10ef9f21d9c72c2d7
Mathieu Westphal <mathieu.westphal@gmail.com>
2022-09-11 13:12:15 +0800

    Adding a simple f3d project

--- versions.cmake
+++ versions.cmake
@@ -39,3 +39,7 @@ superbuild_set_revision(ispc
 superbuild_set_revision(vtk
   URL     "https://www.vtk.org/files/release/9.2/VTK-9.2.0.rc2.tar.gz"
   URL_MD5 f1238c0ad6ed76de03a0f0d570fa3db9)
+
+superbuild_set_revision(f3d
+  URL     "https://github.com/f3d-app/f3d/archive/refs/tags/v1.3.1.tar.gz"
+  URL_MD5 37d126e84853cb6dfc030efdd5b6bf05)
