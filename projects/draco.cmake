7195411edd74a53905c4722db00a21003589490d
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-12-07 23:06:44 +0100

    Fixup macosx

--- projects/draco.cmake
+++ projects/draco.cmake
@@ -8,6 +8,7 @@ superbuild_add_project(draco
     -DDRACO_STANDARD_EDGEBREAKER=OFF
     -DCMAKE_BUILD_TYPE=Release
     -DBUILD_SHARED_LIBS=ON
+    -DCMAKE_MACOSX_RPATH=OFF
 )
 
 superbuild_apply_patch(draco fix-pic "Fix usage of PIC")
