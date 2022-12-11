f060e60919035e6bdc3751f9099b8e831f4d8b47
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-12-11 18:38:18 +0100

    Apple bundle fixup

--- projects/apple/f3d-appname.cmake
+++ projects/apple/f3d-appname.cmake
@@ -2,8 +2,4 @@
 # Sets up a scoped variable named `f3d_appname`.
 include(f3d.info)
 include(f3d-version)
-set(name_suffix "")
-if (f3d_version_branch)
-  set(name_suffix "-${f3d_version_branch}")
-endif ()
-set(f3d_appname "${f3d_name}${name_suffix}-${f3d_version_major}.${f3d_version_minor}.${f3d_version_patch}${f3d_version_suffix}.app")
+set(f3d_appname "${f3d_name}-${f3d_version_major}.${f3d_version_minor}.${f3d_version_patch}${f3d_version_suffix}.app")
