214c15c545de71e621b6affa5823aabf95c3291b
Mathieu Westphal <mathieu.westphal@gmail.com>
2023-02-04 11:49:59 +0100

    F3D CMake options have changed, update them (#38)

--- projects/f3d.cmake
+++ projects/f3d.cmake
@@ -19,7 +19,16 @@ superbuild_add_project(f3d
     -Werror=dev
     -Werror=deprecated
     --warn-uninitialized
-    -DF3D_STRICT_BUILD=ON
+    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_MACOSX_RPATH=OFF
+    -DF3D_INSTALL_DEFAULT_CONFIGURATION_FILE=ON
+    -DF3D_INSTALL_SDK=ON
+    -DF3D_INSTALL_SDK_PLUGIN=OFF
+    -DF3D_LINUX_GENERATE_MAN:BOOL=${f3d_build_for_linux}
+    -DF3D_LINUX_INSTALL_DEFAULT_CONFIGURATION_FILE_IN_PREFIX:BOOL=${f3d_build_for_linux}
+    -DF3D_LINUX_INSTALL_MIME_TYPES_FILES:BOOL=${f3d_build_for_linux}
+    -DF3D_LINUX_INSTALL_THUMBNAILER_FILES:BOOL=${f3d_build_for_linux}
+    -DF3D_MACOS_BUNDLE:BOOL=${f3d_build_for_macos}
     -DF3D_MODULE_EXTERNAL_RENDERING=ON
     -DF3D_MODULE_RAYTRACING:BOOL=${ospray_enabled}
     -DF3D_PLUGIN_BUILD_ALEMBIC:BOOL=${alembic_enabled}
@@ -27,19 +36,10 @@ superbuild_add_project(f3d
     -DF3D_PLUGIN_BUILD_DRACO:BOOL=${draco_enabled}
     -DF3D_PLUGIN_BUILD_OCCT:BOOL=${occt_enabled}
     -DF3D_PYTHON_BINDINGS:BOOL=${pybind11_enabled}
-    -DF3D_INSTALL_SDK=ON
-    -DF3D_INSTALL_SDK_PLUGIN=OFF
-    -DCMAKE_BUILD_TYPE=Release
-    -DF3D_INSTALL_DEFAULT_CONFIGURATION_FILE=ON
-    -DF3D_INSTALL_DEFAULT_CONFIGURATION_FILE_IN_PREFIX:BOOL=${f3d_build_for_linux}
-    -DF3D_GENERATE_MAN:BOOL=${f3d_build_for_linux}
-    -DF3D_INSTALL_MIME_TYPES_FILES:BOOL=${f3d_build_for_linux}
-    -DF3D_INSTALL_THUMBNAILER_FILES:BOOL=${f3d_build_for_linux}
-    -DF3D_MACOS_BUNDLE:BOOL=${f3d_build_for_macos}
-    -DF3D_INSTALL_LOGOS_FOR_NSIS_PACKAGING:BOOL=${f3d_build_for_windows}
+    -DF3D_STRICT_BUILD=ON
     -DF3D_WINDOWS_GUI:BOOL=${f3d_build_for_windows}
+    -DF3D_WINDOWS_INSTALL_LOGOS_FOR_NSIS_PACKAGING:BOOL=${f3d_build_for_windows}
     -DF3D_WINDOWS_RESOURCES_INSTALL_DIR=resources
-    -DCMAKE_MACOSX_RPATH=OFF
   )
 
 # Installing components that are not part of the standard install
