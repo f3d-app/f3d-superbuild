4f95b29c475af368f03c21215e0641fcbae5a2f0
Mathieu Westphal <mathieu.westphal@gmail.com>
2023-04-12 10:54:16 +0200

    Augment components install (#53)
    * Add missing components to install
    
    * remove useless cmake options

--- projects/f3d.cmake
+++ projects/f3d.cmake
@@ -21,12 +21,8 @@ superbuild_add_project(f3d
     --warn-uninitialized
     -DCMAKE_BUILD_TYPE=Release
     -DCMAKE_MACOSX_RPATH=OFF
-    -DF3D_INSTALL_DEFAULT_CONFIGURATION_FILE=ON
-    -DF3D_INSTALL_SDK=ON
     -DF3D_LINUX_GENERATE_MAN:BOOL=${f3d_build_for_linux}
     -DF3D_LINUX_INSTALL_DEFAULT_CONFIGURATION_FILE_IN_PREFIX:BOOL=${f3d_build_for_linux}
-    -DF3D_LINUX_INSTALL_MIME_TYPES_FILES:BOOL=${f3d_build_for_linux}
-    -DF3D_LINUX_INSTALL_THUMBNAILER_FILES:BOOL=${f3d_build_for_linux}
     -DF3D_MACOS_BUNDLE:BOOL=${f3d_build_for_macos}
     -DF3D_MODULE_EXTERNAL_RENDERING=ON
     -DF3D_MODULE_RAYTRACING:BOOL=${ospray_enabled}
@@ -43,6 +39,8 @@ superbuild_add_project(f3d
 # Installing components that are not part of the standard install
 superbuild_project_add_step("extra-install"
   COMMAND ${CMAKE_COMMAND} --install <BINARY_DIR> --component mimetypes
+  COMMAND ${CMAKE_COMMAND} --install <BINARY_DIR> --component configuration
+  COMMAND ${CMAKE_COMMAND} --install <BINARY_DIR> --component sdk
   COMMENT
     "Installing extra components"
   DEPENDEES
