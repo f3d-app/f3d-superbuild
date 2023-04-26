d334492f2949bd6c296c412db8ae5b36a569d5d5
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-04-26 20:55:39 +0800

    disable freetype

--- projects/occt.cmake
+++ projects/occt.cmake
@@ -1,20 +1,13 @@
 set(occt_toolkits TKSTEP TKIGES TKMesh TKXDESTEP TKXDEIGES)
 list(JOIN occt_toolkits "${_superbuild_list_separator}" occt_toolkits_escaped)
 
-set(occt_depends)
-if (UNIX AND NOT APPLE)
-  list(APPEND occt_depends
-    fontconfig)
-endif ()
-
 superbuild_add_project(occt
   LICENSE_FILES
     LICENSE_LGPL_21.txt
     OCCT_LGPL_EXCEPTION.txt
-  DEPENDS freetype ${occt_depends}
   CMAKE_ARGS
-    -DINSTALL_DIR_BIN:PATH=bin
     -DBUILD_ADDITIONAL_TOOLKITS=${occt_toolkits_escaped}
+    -DBUILD_DOC_Overview=OFF
     -DBUILD_MODULE_ApplicationFramework=OFF
     -DBUILD_MODULE_DataExchange=OFF
     -DBUILD_MODULE_Draw=OFF
@@ -22,7 +15,8 @@ superbuild_add_project(occt
     -DBUILD_MODULE_ModelingAlgorithms=OFF
     -DBUILD_MODULE_ModelingData=OFF
     -DBUILD_MODULE_Visualization=OFF
-    -DBUILD_DOC_Overview=OFF
     -DCMAKE_BUILD_TYPE=Release
     -DCMAKE_INSTALL_RPATH=<INSTALL_DIR>/lib
+    -DINSTALL_DIR_BIN:PATH=bin
+    -DUSE_FREETYPE=OFF
 )
