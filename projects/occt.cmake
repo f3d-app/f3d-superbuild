1d533c66eb6e59229c521891385f804f9852f114
Mathieu Westphal <mathieu.westphal@gmail.com>
2023-04-30 22:18:16 +0800

    Add coloring support for step files (#60)
    Addd coloring for step files on Windows and Linux

--- projects/occt.cmake
+++ projects/occt.cmake
@@ -1,4 +1,11 @@
+option(OCCT_ENABLE_COLORING "Enable Coloring modules in OCCT" ON)
+mark_as_advanced(OCCT_ENABLE_COLORING)
+
 set(occt_toolkits TKSTEP TKIGES TKMesh)
+if (OCCT_ENABLE_COLORING)
+  list(APPEND occt_toolkits TKXDESTEP TKXDEIGES)
+endif ()
+
 list(JOIN occt_toolkits "${_superbuild_list_separator}" occt_toolkits_escaped)
 
 superbuild_add_project(occt
@@ -6,8 +13,8 @@ superbuild_add_project(occt
     LICENSE_LGPL_21.txt
     OCCT_LGPL_EXCEPTION.txt
   CMAKE_ARGS
-    -DINSTALL_DIR_BIN:PATH=bin
     -DBUILD_ADDITIONAL_TOOLKITS=${occt_toolkits_escaped}
+    -DBUILD_DOC_Overview=OFF
     -DBUILD_MODULE_ApplicationFramework=OFF
     -DBUILD_MODULE_DataExchange=OFF
     -DBUILD_MODULE_Draw=OFF
@@ -15,7 +22,8 @@ superbuild_add_project(occt
     -DBUILD_MODULE_ModelingAlgorithms=OFF
     -DBUILD_MODULE_ModelingData=OFF
     -DBUILD_MODULE_Visualization=OFF
-    -DBUILD_DOC_Overview=OFF
     -DCMAKE_BUILD_TYPE=Release
     -DCMAKE_INSTALL_RPATH=<INSTALL_DIR>/lib
+    -DINSTALL_DIR_BIN:PATH=bin
+    -DUSE_FREETYPE=OFF
 )
