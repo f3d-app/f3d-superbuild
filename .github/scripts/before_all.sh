56c131fcc5a2ae10bdd7ad0d2c503a17e989d467
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-09-04 07:57:46 +0200

    only openexr

--- .github/scripts/before_all.sh
+++ .github/scripts/before_all.sh
@@ -8,19 +8,19 @@ cmake -S f3d-superbuild -B f3d-superbuild-build \
         -DBUILD_SHARED_LIBS=OFF \
         -DCMAKE_BUILD_TYPE=Release \
         -DENABLE_f3d=OFF \
-        -DENABLE_alembic=ON \
-        -DENABLE_assimp=ON \
-        -DENABLE_draco=ON \
-        -DENABLE_occt=ON \
+        -DENABLE_alembic=OFF \
+        -DENABLE_assimp=OFF \
+        -DENABLE_draco=OFF \
+        -DENABLE_occt=OFF \
         -DENABLE_openexr=ON \
         -DENABLE_ospray=OFF \
-        -DENABLE_pybind11=ON \
-        -DENABLE_tbb=ON \
-        -DENABLE_vtk=ON \
-        -DOCCT_ENABLE_COLORING=ON \
+        -DENABLE_pybind11=OFF \
+        -DENABLE_tbb=OFF \
+        -DENABLE_vtk=OFF \
+        -DOCCT_ENABLE_COLORING=OFF \
         -Dvtk_SOURCE_SELECTION=git \
         -Dvtk_GIT_TAG=$1
 
-# cmake --build f3d-superbuild-build
+cmake --build f3d-superbuild-build
 
 sccache --stop-server
