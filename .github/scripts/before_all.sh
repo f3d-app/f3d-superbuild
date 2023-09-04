e094165be425f79330d5eaea7df0c7abf23bdfb6
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-09-04 07:52:27 +0200

    tartstop

--- .github/scripts/before_all.sh
+++ .github/scripts/before_all.sh
@@ -3,24 +3,4 @@
 set -e -u
 
 sccache --start-server
-
-cmake -S f3d-superbuild -B f3d-superbuild-build \
-        -DBUILD_SHARED_LIBS=OFF \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DENABLE_f3d=OFF \
-        -DENABLE_alembic=ON \
-        -DENABLE_assimp=ON \
-        -DENABLE_draco=ON \
-        -DENABLE_occt=ON \
-        -DENABLE_openexr=ON \
-        -DENABLE_ospray=OFF \
-        -DENABLE_pybind11=ON \
-        -DENABLE_tbb=ON \
-        -DENABLE_vtk=ON \
-        -DOCCT_ENABLE_COLORING=ON \
-        -Dvtk_SOURCE_SELECTION=git \
-        -Dvtk_GIT_TAG=$1
-
-cmake --build f3d-superbuild-build
-
 sccache --stop-server
