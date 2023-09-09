572a5b9a7703d848e6b733617ccc9b0367d7af96
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-09-09 12:59:41 +0200

    Factorize before build

---
+++ .github/actions/wheels/build_f3d_superbuild.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cmake -S ./source -B ./fsbb      \
+  -DBUILD_SHARED_LIBS=OFF    \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DENABLE_f3d=OFF           \
+  -DENABLE_alembic=ON        \
+  -DENABLE_assimp=ON         \
+  -DENABLE_draco=ON          \
+  -DENABLE_occt=ON           \
+  -DENABLE_openexr=ON        \
+  -DENABLE_ospray=OFF        \
+  -DENABLE_pybind11=ON       \
+  -DENABLE_tbb=ON            \
+  -DENABLE_vtk=ON            \
+  -DOCCT_ENABLE_COLORING=ON  \
+  -Dvtk_SOURCE_SELECTION=git \
+  -Dvtk_GIT_TAG=${{ env.VTK_COMMIT_SHA }}
+cmake --build ./fsbb
