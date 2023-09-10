f2691615edd0bf523e36da797c79a2923cb3b9a4
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-09-10 23:13:48 +0200

    try again

--- .github/actions/wheels/copy_mesa.sh
+++ .github/actions/wheels/copy_mesa.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 python_bin_dir=`python -c 'import sys;from os.path import dirname; print(dirname(sys.executable))'`
-echo $python_bin_dir
-ls $python_bin_dir
-which python
-which pytest
 cp $1/mesa/x64/opengl32.dll $python_bin_dir
 cp $1/mesa/x64/libglapi.dll $python_bin_dir
 cp $1/mesa/x64/libgallium_wgl.dll $python_bin_dir
@@ -14,3 +10,4 @@ cp $1/mesa/x64/libgallium_wgl.dll $2/bin
 cp $1/mesa/x64/opengl32.dll $2
 cp $1/mesa/x64/libglapi.dll $2
 cp $1/mesa/x64/libgallium_wgl.dll $2
+ls $python_bin_dir
