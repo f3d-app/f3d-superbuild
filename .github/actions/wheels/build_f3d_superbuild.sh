8bac5779a8766963da06171e58afb00a6a42d698
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-09-11 07:28:27 +0200

    cxx11

--- .github/actions/wheels/build_f3d_superbuild.sh
+++ .github/actions/wheels/build_f3d_superbuild.sh
@@ -14,6 +14,6 @@ cmake -S ./source -B ./fsbb      \
   -DENABLE_vtk=ON            \
   -DOCCT_ENABLE_COLORING=ON  \
   -Dvtk_SOURCE_SELECTION=git \
-  -DCMAKE_CXX_FLAGS='-D_GLIBCXX_USE_CXX11_ABI=0' \
+  -DCMAKE_CXX_FLAGS='-D_GLIBCXX_USE_CXX11_ABI=1' \
   -Dvtk_GIT_TAG=$1
 cmake --build ./fsbb
