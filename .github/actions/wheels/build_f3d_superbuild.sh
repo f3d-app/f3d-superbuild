c9a4f74ce5e8c285986871a6b12467243f4fdfd2
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-09-16 11:05:10 +0200

    Build all

--- .github/actions/wheels/build_f3d_superbuild.sh
+++ .github/actions/wheels/build_f3d_superbuild.sh
@@ -3,17 +3,17 @@ cmake -S ./source -B ./fsbb      \
   -DBUILD_SHARED_LIBS=OFF    \
   -DCMAKE_BUILD_TYPE=Release \
   -DENABLE_f3d=OFF           \
-  -DENABLE_alembic=OFF        \
-  -DENABLE_assimp=OFF         \
-  -DENABLE_draco=OFF          \
-  -DENABLE_occt=OFF         \
-  -DENABLE_openexr=OFF        \
+  -DENABLE_alembic=ON        \
+  -DENABLE_assimp=ON         \
+  -DENABLE_draco=ON          \
+  -DENABLE_occt=ON         \
+  -DENABLE_openexr=ON        \
   -DENABLE_ospray=OFF        \
   -DENABLE_pybind11=ON       \
-  -DENABLE_tbb=OFF            \
+  -DENABLE_tbb=ON            \
   -DENABLE_vtk=ON            \
-  -DOCCT_ENABLE_COLORING=OFF  \
-  -DUSE_SYSTEM_python3=OFF    \
+  -DOCCT_ENABLE_COLORING=ON  \
+  -DUSE_SYSTEM_python3=ON    \
   -Dvtk_SOURCE_SELECTION=git \
   -Dvtk_GIT_TAG=$1
 cmake --build ./fsbb
