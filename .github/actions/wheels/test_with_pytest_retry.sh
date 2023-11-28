e9bc3f4514bfaa4f6bd3c9b8f00bdf15ef729fed
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-11-28 08:52:21 +0100

    new version

--- .github/actions/wheels/test_with_pytest_retry.sh
+++ .github/actions/wheels/test_with_pytest_retry.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-# Run pytest command two times if needed to pass
-max_retry=2
+
+# Recover python version major.minor
+python_version=`python --version`
+python_version=`cut -d ' ' -f 2 <<< "$python_version"`
+python_version=`cut -d '.' -f 1,2 <<< "$python_version"`
+
+# Recover system information
+machine=`uname -s`
+
+# Try once in general
+# Twice with macOS and python 3.12 to work around this issue:
+# https://github.com/pytest-dev/pytest/issues/11647
+max_retry=1
+if test "$python_version" = "3.12"
+then
+  if test "$machine" = "Darwin"
+  then
+    max_retry=2
+  fi
+fi
+
+# Run pytest command multiple times if needed to pass and it failed with -11
+echo "Trying pytest a maximum of $max_retry times"
 counter=0
-until pytest -s $1/python/testing 
+until [ "$counter" -ge $max_retry ]
 do
-   [[ counter -eq $max_retry ]] && echo "Failed!" && exit 1
-   echo "Trying again. Try #$counter"
+   pytest -s $1/python/testing
+   retVal=$?
+   if [ $retVal -ne -11 ]; then
+     exit $retVal
+   fi
    ((counter++))
 done
