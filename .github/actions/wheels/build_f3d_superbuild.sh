69c047e4527e65c5dc86435d3bc348ae0b582d04
Michael Migliore <mcmigliore@gmail.com>
2023-12-16 17:00:31 +0100

    osx target

--- .github/actions/wheels/build_f3d_superbuild.sh
+++ .github/actions/wheels/build_f3d_superbuild.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-cmake -S ./source -B ./fsbb      \
-  -DBUILD_SHARED_LIBS=OFF    \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DENABLE_f3d=OFF           \
-  -DENABLE_alembic=ON        \
-  -DENABLE_assimp=ON         \
-  -DENABLE_draco=ON          \
-  -DENABLE_occt=ON           \
-  -DENABLE_openexr=ON        \
-  -DENABLE_openusd=ON        \
-  -DENABLE_ospray=OFF        \
-  -DENABLE_pybind11=ON       \
-  -DENABLE_tbb=ON            \
-  -DENABLE_vtk=ON            \
-  -DOCCT_ENABLE_COLORING=ON  \
-  -DUSE_SYSTEM_python3=ON    \
-  -Dvtk_SOURCE_SELECTION=git \
+cmake -S ./source -B ./fsbb           \
+  -DBUILD_SHARED_LIBS=OFF             \
+  -DCMAKE_BUILD_TYPE=Release          \
+  -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
+  -DENABLE_f3d=OFF                    \
+  -DENABLE_alembic=ON                 \
+  -DENABLE_assimp=ON                  \
+  -DENABLE_draco=ON                   \
+  -DENABLE_occt=ON                    \
+  -DENABLE_openexr=ON                 \
+  -DENABLE_openusd=ON                 \
+  -DENABLE_ospray=OFF                 \
+  -DENABLE_pybind11=ON                \
+  -DENABLE_tbb=ON                     \
+  -DENABLE_vtk=ON                     \
+  -DOCCT_ENABLE_COLORING=ON           \
+  -DUSE_SYSTEM_python3=ON             \
+  -Dvtk_SOURCE_SELECTION=git          \
   -Dvtk_GIT_TAG=$1
 cmake --build ./fsbb
