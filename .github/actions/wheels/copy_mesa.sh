691c4d8950cb2d74fd1e524da488d328dcb05e98
Mathieu Westphal <mathieu.westphal@gmail.com>
2024-04-24 09:22:45 +0200

    Use mesa action (#172)

--- .github/actions/wheels/copy_mesa.sh
+++ .github/actions/wheels/copy_mesa.sh
@@ -3,6 +3,6 @@
 # Copy mesa to the right location, next to the future location of libf3d python module
 python_bin_dir=`python -c 'import sys;from os.path import dirname; print(dirname(sys.executable))'`
 mkdir $python_bin_dir/../Lib/site-packages/f3d/
-cp $1/mesa/x64/opengl32.dll $python_bin_dir/../Lib/site-packages/f3d/
-cp $1/mesa/x64/libglapi.dll $python_bin_dir/../Lib/site-packages/f3d/
-cp $1/mesa/x64/libgallium_wgl.dll $python_bin_dir/../Lib/site-packages/f3d
+cp $1/mesa/opengl32.dll $python_bin_dir/../Lib/site-packages/f3d/
+cp $1/mesa/libglapi.dll $python_bin_dir/../Lib/site-packages/f3d/
+cp $1/mesa/libgallium_wgl.dll $python_bin_dir/../Lib/site-packages/f3d
