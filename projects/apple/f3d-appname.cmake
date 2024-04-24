67db82ad4342a03bb9ee34f9c945a11bce56da52
Michael Migliore <mcmigliore@gmail.com>
2024-04-24 20:49:34 +0200

    MacOS .dmg fixes

--- projects/apple/f3d-appname.cmake
+++ projects/apple/f3d-appname.cmake
@@ -2,4 +2,4 @@
 # Sets up a scoped variable named `f3d_appname`.
 include(f3d.info)
 include(f3d-version)
-set(f3d_appname "${f3d_name}-${f3d_version_major}.${f3d_version_minor}.${f3d_version_patch}${f3d_version_suffix}.app")
+set(f3d_appname "${f3d_name}.app")
