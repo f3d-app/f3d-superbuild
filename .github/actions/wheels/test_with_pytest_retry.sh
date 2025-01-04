24f0f4ef4661a0f0335770337104adab545f7ea1
Mathieu Westphal <mathieu.westphal@kitware.com>
2025-01-04 10:23:19 +0100

    Reapply "test again"
    This reverts commit 5061015b41f721b37aa01a5138f2dd7069c94804.

--- .github/actions/wheels/test_with_pytest_retry.sh
+++ .github/actions/wheels/test_with_pytest_retry.sh
@@ -1,3 +1,44 @@
 #!/bin/bash
+set -uo pipefail
 
-exit 0
+# Get Python major.minor version
+python_version=$(python --version)
+python_version=${python_version#* }
+python_version=${python_version%.*}
+echo "Python version: $python_version"
+
+# Recover system information
+machine=$(uname -s)
+
+# Try once in general
+# Ten time with macOS and python 3.12
+max_retry=1
+if [[ $python_version == '3.12' && $machine == 'Darwin' ]]
+then
+  max_retry=10
+fi
+
+# Run pytest command multiple times if needed to pass
+echo "Trying pytest a maximum of $max_retry times"
+counter=0
+while [[ $counter -lt $max_retry ]]
+do
+  # Run pytest and recover result
+  pytest -s "$1/python/testing"
+  result=$?
+  echo $result
+
+  # Retry only if result is 139 (segfault)
+  if [[ result -eq 139 ]]
+  then
+   ((counter++))
+   echo "Try $counter failed! Tries remaining: $((max_retry - counter))"
+   continue
+  else
+    echo "Not retrying"
+    exit $result
+  fi
+done
+
+# max_retry reached, exit in failure
+exit 1
