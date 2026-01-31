c8c94b21330ca2c9ef3974c46a5768bf6df65fdb
Jean Fechter <jean.fechter@gmail.com>
2026-01-31 15:38:58 +0100

    Patch webifc c++20 usage

--- projects/webifc.cmake
+++ projects/webifc.cmake
@@ -5,3 +5,6 @@ superbuild_add_project(webifc
   SOURCE_SUBDIR src/cpp
   CMAKE_ARGS
     -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON)
+
+superbuild_apply_patch(webifc remove-std-format
+  "Replace std::format with std::to_chars for compiler compatibility")
